afdffde28c41a379563f254f7b2f47af9a8ef4df
Miguel Rivas <mrivaslezma@gmail.com>
2025-04-04 22:22:32 -0300

    working basic

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_05_004441) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_05_010828) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.string "last_name"
+    t.string "rut"
+    t.string "phone"
+    t.string "location"
+    t.string "avatar"
+    t.bigint "user_id", null: false
+    t.boolean "done"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +39,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_05_004441) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "lists", "users"
 end
